1b1b68a18369f2f94d1223975db01e91757a7e56
finding-archit <archit.gupta865@gmail.com>
2026-04-30 02:51:42 +0530

    Update title in main.py

--- app/main.py
+++ app/main.py
@@ -189,7 +189,7 @@ def index():
 <head>
   <meta charset="UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>Hello</title>
+  <title>Weather App</title>
   <style>
     * { box-sizing: border-box; margin: 0; padding: 0; }
     body {
